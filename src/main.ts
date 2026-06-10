76a3a6d09f14024f03e3efb7de31626a89492d79
Uchiha-Ichi <hoang290603@gmail.com>
2026-06-10 16:24:21 +0700

    fix: add domain

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       process.env.FRONTEND_URL,
+      'https://matcha.net.vn',
       'https://matcha-fe-2uwg.vercel.app',
       'http://localhost:5173',
       'http://localhost:5174',
